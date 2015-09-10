6e62e429915351c3b093c2a0a0232cfe50799424
Christian Pschorr <pschorr.christian@gmail.com>
2015-09-10 17:38:49 +0200

    Add outer layout class

--- src/Cms/Views/page/index.blade.php
+++ src/Cms/Views/page/index.blade.php
@@ -1,28 +1,30 @@
 @include('cms::header')
 
-<div class="inner">
-	<div class="row show-grid">
-		<div class="col-xs-4">
-			<div class="node node-container">
-				@foreach($pages as $page)
-					<div class="node">
-						<div><a href="{{route('page', ['page' => $page->id])}}">{{$page->title}}</a></div>
-					</div>
-				@endforeach
+<div class="outer">
+	<div class="inner">
+		<div class="row show-grid">
+			<div class="col-xs-4">
+				<div class="node node-container">
+					@foreach($pages as $page)
+						<div class="node">
+							<div><a href="{{route('page', ['page' => $page->id])}}">{{$page->title}}</a></div>
+						</div>
+					@endforeach
 
-				<div><a href="{{route('node.create')}}" class="node-create"></a></div>
+					<div><a href="{{route('node.create')}}" class="node-create"></a></div>
+				</div>
 			</div>
-		</div>
-		<div class="col-xs-8">
-			<div class="node node-container">
-				@foreach($nodes as $node)
-					<div class="node">
-						<header>{{$node->name}}</header>
-						<div><a href="{{route('node.form', ['node' => $node->id])}}">Bearbeiten</a></div>
-					</div>
-				@endforeach
+			<div class="col-xs-8">
+				<div class="node node-container">
+					@foreach($nodes as $node)
+						<div class="node">
+							<header>{{$node->name}}</header>
+							<div><a href="{{route('node.form', ['node' => $node->id])}}">Bearbeiten</a></div>
+						</div>
+					@endforeach
 
-				<div><a href="{{route('node.create')}}" class="node-create"></a></div>
+					<div><a href="{{route('node.create')}}" class="node-create"></a></div>
+				</div>
 			</div>
 		</div>
 	</div>
