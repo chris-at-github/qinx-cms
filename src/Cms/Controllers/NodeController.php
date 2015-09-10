30dc37ec1c8054ec652b314dcb67aa46ea415e82
Christian Pschorr <pschorr.christian@gmail.com>
2015-09-10 07:00:19 +0200

    Remove copy and paste error

--- src/Cms/Controllers/NodeController.php
+++ src/Cms/Controllers/NodeController.php
@@ -1,6 +1,11 @@
 <?php namespace Cms\Controllers;
 
 class NodeController extends CmsController {
+
+	/**
+	 * @param $nodeType \Cms\Models\NodeType
+	 * @return mixed
+	 */
 	public function create($nodeType = null) {
 		$node				= null;
 		$nodeTypes 	= \App::make('\Cms\Repositories\NodeTypeRepository');
@@ -19,17 +24,25 @@ class NodeController extends CmsController {
 			->with('types', $nodeTypes->findall());
 	}
 
+	/**
+	 * @param $node \Cms\Models\Node
+	 * @return \Illuminate\Contracts\View\View
+	 */
 	public function form($node) {
 		return \View::make('cms::node.form')
 			->with('node', $node)
 			->with('types', \App::make('\Cms\Repositories\NodeTypeRepository')->findall());
 	}
 
+	/**
+	 * @param $node \Cms\Models\Node
+	 * @return \Illuminate\Support\Facades\Redirect
+	 */
 	public function store($node) {
 		if($node->store(\Input::all()) === false) {
 			return \Redirect::back()
 				->withInput()
-				->withErrors($map->errors());
+				->withErrors($node->errors());
 		}
 
 		return \Redirect::to('/');
